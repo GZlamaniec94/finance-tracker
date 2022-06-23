2e7ced15e46e521a20c8af04c16c68f7623a089c
Grzegorz Złamaniec <g.zlamaniec@gmail.com>
2022-06-23 14:20:49 +0200

    Added following and unfollowing friends

---
+++ app/controllers/friendships_controller.rb
@@ -0,0 +1,22 @@
+class FriendshipsController < ApplicationController
+  
+  def create
+    friend = User.find(params[:friend])
+    current_user.friendships.build(friend_id: friend.id)
+    if current_user.save
+      flash[:notice] = "Added to friends list"
+    else
+      flash[:alert] = "Somehing went wrong"
+    end    
+    redirect_to my_friends_path
+  end
+  
+  
+  def destroy
+    friendship = current_user.friendships.where(friend_id: params[:id]).first
+    friendship.destroy
+    flash[:notice] = "Removed from friends"
+    redirect_to my_friends_path
+  end  
+  
+end  
